89eb9ca62661fce5ea8ded697902352de1029e24
746hughr <hughrj@farmingdale.edu>
2025-10-24 20:45:36 -0400

    Designed Registration and Dashboard

--- src/main/java/group_5/banking_system_application/HelloApplication.java
+++ src/main/java/group_5/banking_system_application/HelloApplication.java
@@ -11,7 +11,7 @@ public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("/group_5/banking_system_application/FXML Layouts/login-page.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        Scene scene = new Scene(fxmlLoader.load(), 600, 600);
         stage.setTitle("Login or Sign Up");
         stage.setScene(scene);
         stage.show();
