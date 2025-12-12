fff39eed9e1b57de02c50a96938ce6ed6920590c
tombour <ridgerouzier@gmail.com>
2025-12-12 18:16:07 -0500

    Beneficiaries now show can be added to firebase and show up into beneficiaries page

--- src/main/java/group_5/banking_system_application/Ui/BeneficiaryController.java
+++ src/main/java/group_5/banking_system_application/Ui/BeneficiaryController.java
@@ -1,5 +1,10 @@
 package group_5.banking_system_application.Ui;
 
+import com.google.api.core.ApiFuture;
+import com.google.cloud.firestore.Firestore;
+import com.google.cloud.firestore.QueryDocumentSnapshot;
+import com.google.cloud.firestore.QuerySnapshot;
+import com.google.firebase.cloud.FirestoreClient;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.collections.transformation.FilteredList;
@@ -10,9 +15,20 @@ import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
 import javafx.scene.control.cell.PropertyValueFactory;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.ExecutionException;
+
 public class BeneficiaryController {
 
     private String currentUserId;
+    private static final Firestore db = FirestoreClient.getFirestore();
+    private static int idIndex = 0;
+
+    //dumb function to generate random ids
+    private static String genBenID() {
+        return "B" + idIndex++;
+    }
 
     @FXML
     private Node root;
@@ -54,11 +70,37 @@ public class BeneficiaryController {
         emailColumn.setCellValueFactory(new PropertyValueFactory<>("email"));
 
         // Dummy data for now
+        /*
         beneficiaries.addAll(
                 new Beneficiary("B001", "John Doe", "ACC12345", "john@mail.com"),
                 new Beneficiary("B002", "Sarah Khan", "ACC98765", "sarah@gmail.com"),
                 new Beneficiary("B003", "Steve R", "ACC23145", "xyz@yahoo.com")
         );
+
+         */
+        ApiFuture<QuerySnapshot> userFuture =
+                db.collection("users")
+                        .whereEqualTo("email", LoginController.globalUserEmail)
+                        .get();
+        try {
+            List<QueryDocumentSnapshot> userDoc = userFuture.get().getDocuments();
+            var userQuery = userDoc.getFirst();
+            if(userQuery.get("beneficiaries") != null) {
+                var bens = (ArrayList<String>) userQuery.get(("beneficiaries"));
+                for (var b : bens) {
+                    ApiFuture<QuerySnapshot> bFuture =
+                            db.collection("users")
+                                    .whereEqualTo("email", b)
+                                    .get();
+                    List<QueryDocumentSnapshot> bDoc = bFuture.get().getDocuments();
+                    var bQuery = bDoc.getFirst();
+                    beneficiaries.add(new Beneficiary(genBenID(),bQuery.get("firstName") + " "
+                    + bQuery.get("lastName"),"",b));
+                }
+            }
+        }catch(ExecutionException | InterruptedException ee) {
+
+        }
 
         beneficiaryTable.setItems(beneficiaries);
         NotificationDialog.initialize(root);
